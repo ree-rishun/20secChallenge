ebe2f03fe2a7c1e10e1890db60e680a4aad63227
ree-rishun <ree0432@gmail.com>
2021-01-01 17:08:10 +0900

    htmlテンプレートの作成

--- back/main.go
+++ back/main.go
@@ -3,58 +3,65 @@ package main
 import (
 	"encoding/json"
 	"github.com/gorilla/mux"
+	"html/template"
 	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
 )
 
-// Book構造体
-
-type Book struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Author *Author `json:"author"`
+// 絵の構造体
+type Picture struct {
+	ID		string  `json:"id"`
+	Title	string  `json:"title"`
+	Path	string	`json:"path"`
 }
 
-type Author struct {
-	FirstName string `json:"firstname"`
-	LastName  string `json:"lastname"`
-}
-
-// Init Books var as a slice Book struct
-var books []Book
+// 絵の格納用配列
+var pictures []Picture
 
 // Get All Books
 func getBooks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(books)
+	json.NewEncoder(w).Encode(pictures)
 }
 
-// Get Single Book
-func getBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+// 特定の絵の取得
+func getPicture(w http.ResponseWriter, r *http.Request) {
+	// ヘッダをセット
+	t, err := template.ParseFiles("template/index.html")
+
+	// エラーの場合
+	if err != nil {
+		log.Fatalf("template error: %v", err)
+	}
+
 	params := mux.Vars(r)
 
-	// Loop through books and find with id
-	for _, item := range books {
+	// 一致するものを取得（仮の処理）
+	for _, item := range pictures {
 		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
+			err = t.Execute(w, item)
+
+			if err != nil {
+				log.Printf("failed to execute template: %v", err)
+			}
 			return
 		}
 	}
-	json.NewEncoder(w).Encode(&Book{})
+	json.NewEncoder(w).Encode(&Picture{})
+
 }
 
 // Create a Book
 func createBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	var book Book
-	_ = json.NewDecoder(r.Body).Decode(&book)
-	book.ID = strconv.Itoa(rand.Intn(10000)) // Mock ID - not safe in production
-	books = append(books, book)
-	json.NewEncoder(w).Encode(book)
+	var picture Picture
+	_ = json.NewDecoder(r.Body).Decode(&picture)
+	picture.ID = strconv.Itoa(rand.Intn(10000)) // Mock ID - not safe in production
+	pictures = append(pictures, picture)
+	json.NewEncoder(w).Encode(picture)
 }
 
 // Update a Book
@@ -63,18 +70,18 @@ func updateBook(w http.ResponseWriter, r *http.Request) {
 
 	params := mux.Vars(r)
 
-	for index, item := range books {
+	for index, item := range pictures {
 		if item.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
-			var book Book
-			_ = json.NewDecoder(r.Body).Decode(&book)
-			book.ID = params["id"]
-			books = append(books, book)
-			json.NewEncoder(w).Encode(book)
+			pictures = append(pictures[:index], pictures[index+1:]...)
+			var picture Picture
+			_ = json.NewDecoder(r.Body).Decode(&picture)
+			picture.ID = params["id"]
+			pictures = append(pictures, picture)
+			json.NewEncoder(w).Encode(picture)
 			return
 		}
 	}
-	json.NewEncoder(w).Encode(books)
+	json.NewEncoder(w).Encode(pictures)
 }
 
 // Delete a Book
@@ -83,13 +90,13 @@ func deleteBook(w http.ResponseWriter, r *http.Request) {
 
 	params := mux.Vars(r)
 
-	for index, item := range books {
+	for index, item := range pictures {
 		if item.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
+			pictures = append(pictures[:index], pictures[index+1:]...)
 			break
 		}
 	}
-	json.NewEncoder(w).Encode(books)
+	json.NewEncoder(w).Encode(pictures)
 }
 
 func main() {
@@ -97,15 +104,16 @@ func main() {
 	r := mux.NewRouter()
 
 	// Mock Data
-	books = append(books, Book{ID: "1", Title: "Book one", Author: &Author{FirstName: "Philip", LastName: "Williams"}})
-	books = append(books, Book{ID: "2", Title: "Book Two", Author: &Author{FirstName: "John", LastName: "Johnson"}})
+	pictures = append(pictures, Picture{ID: "1", Title: "Book one"})
+	pictures = append(pictures, Picture{ID: "2", Title: "Book Two"})
 
 	// Route Hnadlers / Endpoints
-	r.HandleFunc("/api/books", getBooks).Methods("GET")
-	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
-	r.HandleFunc("/api/books", createBook).Methods("POST")
-	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/", getBooks).Methods("GET")
+	r.HandleFunc("/challenge/{id}", getPicture).Methods("GET")
+	r.HandleFunc("/challenge", createBook).Methods("POST")
+	r.HandleFunc("/gallery/{id}", updateBook).Methods("PUT")
 	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 
+	// ポート指定
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
